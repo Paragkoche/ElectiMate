0f57c069fb7b90918ba7061204620f815e81ae3b
Parag koche <60149545+Paragkoche@users.noreply.github.com>
2025-04-28 11:59:43 +0000

    init

---
+++ app/src/utils/supabase/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { createClient } from "./client-ssr";
+
+export async function updateSession(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
+
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith("/login") &&
+    !request.nextUrl.pathname.startsWith("/auth")
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  return supabaseResponse;
+}
